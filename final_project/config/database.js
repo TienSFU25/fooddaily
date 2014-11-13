1db36021f32a5138f62d5b733e92b2a2f8fd278f
Tien <tienv@sfu.ca>
2014-11-12 21:58:18 -0800

    added food model and methods to database file

--- final_project/config/database.js
+++ final_project/config/database.js
@@ -5,7 +5,7 @@ var sequelize = new Sequelize('groupdb', 'group', 'thisgrouprocks', {
 	host: 'localhost',
 	dialect: 'mysql',
 	language: 'en',
-	// logging: false
+	logging: false
 })
 
 var User = sequelize.define('User2', {
@@ -15,6 +15,12 @@ var User = sequelize.define('User2', {
 	description: {type: Sequelize.STRING}
 })
 
+var Food = sequelize.define('Food', {
+	foodid: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+	foodname: {type: Sequelize.STRING, allowNull: false},
+})
+
+User.hasMany(Food)
 // sequelize.sync({force: true})
 
 Database.prototype.createUser = function f(username, password, callback) {
@@ -29,6 +35,55 @@ Database.prototype.createUser = function f(username, password, callback) {
 
 Database.prototype.searchUser = function f(username, callback) {
 	User.findOne({where: {username: username}}).success(callback)
+}
+
+Database.prototype.addFood = function f(userid, foodname, callback) {
+	User.findOne(userid).success(function(user) {
+	var f = Food.build({
+		foodname: foodname
+	})
+		user.addFood(f)
+	})
+
+	callback()
+}
+
+Database.prototype.getAllFoods = function f(userid, callback) {
+	User.findOne(userid).success(function(user) {
+		user.getFoods().success(callback)
+	})
+}
+
+Database.prototype.getFood = function f(userid, foodid, callback) {
+	User.findOne(userid).success(function(user) {
+		user.getFoods({where: {foodid: foodid}}).success(callback)
+	})
+}
+
+// TODO: Hard coded update food field. change this
+Database.prototype.updateFood = function f(userid, foodid, newFoodName, callback) {
+	Food.update(
+		{foodname: newFoodName}
+	,
+	{
+		where: {
+			User2userid: userid,
+			foodid: foodid
+		}
+	}).success(callback)
+}
+
+Database.prototype.deleteFood = function f(userid, foodid, callback) {
+	User.findOne(userid).success(function(user) {
+		user.removeFood(foodid)
+		Food.destroy(
+		{
+			where: {
+				User2userid: userid,
+				foodid: foodid
+			}
+		}).success(callback)
+	})
 }
 
 module.exports = Database
