de0c381cefa6c2c9fb144c87edfb92ae90aeaf59
Tien <tienv@sfu.ca>
2014-11-16 17:45:23 -0800

    pie chart for clicking the table

--- final_project/public/javascript/script.js
+++ final_project/public/javascript/script.js
@@ -30,19 +30,38 @@ var queryDict = {
   }
 }
 
-function searchFoodById() {
-	xmlhttp = new XMLHttpRequest();
+var nutrition = [
+	"nf_calories",
+	"nf_total_fat",
+	"nf_total_carbohydrate",
+	"nf_protein",
+	"nf_sodium"
+]
 
-	xmlhttp.onreadystatechange=function()
-	  {
-	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	    {
-			$foodOut.val(xmlhttp['responseText'])
-	    }
-	  }
-	xmlhttp.open("get", "https://api.nutritionix.com/v1_1/item?id=5266a0fa9f05a39eb3007777&appId="
-						+appId+"&appKey="+appKey, true)
-	xmlhttp.send()
+// function searchFoodById(foodid) {
+// 	xmlhttp = new XMLHttpRequest();
+
+// 	xmlhttp.onreadystatechange=function()
+// 	  {
+// 	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+// 	    {
+// 			googlePieUpdate(xmlhttp['responseText'])
+// 	    }
+// 	  }
+// 	xmlhttp.open("get",, true)
+// 	xmlhttp.send()
+// }
+// requests nutritionix, parse json and update google table
+
+function searchFoodById(foodid) {
+	$.ajax({
+		type: "GET",
+		url: "https://api.nutritionix.com/v1_1/item?id=" + foodid 
+		+"&appId="+appId+"&appKey="+appKey,
+		success: function(d) {
+			googlePieUpdate(d)
+		},
+	})
 }
 
 // requests nutritionix, parse json and update google table
@@ -62,8 +81,10 @@ function searchFoodsByName(foodName) {
 // use google charts to draw a table
 google.load("visualization", "1", {packages:["corechart", "table"]});
 google.setOnLoadCallback(googleChartsInit)
-var googleData;
+var tableData;
+var pieData;
 var googleTable;
+var googlePieChart;
 
 var fields = queryDict['fields']
 
@@ -71,34 +92,38 @@ var fields = queryDict['fields']
 // initialize the columns. use all string fields
 
 // TODO: better way to update table than restarting?
-function resetData() {
-	googleData = new google.visualization.DataTable()
+function resetTableData() {
+	tableData = new google.visualization.DataTable()
  	for (var i = 0; i < fields.length; i++) {
- 		googleData.addColumn('string', fields[i])
+ 		tableData.addColumn('string', fields[i])
  	}
 }
 
-// initialize an empty table
+// initialize an empty table and pie chart
 function googleChartsInit() {
 	googleTable = new google.visualization.Table($('.foodChart').get(0));
+	googlePieChart = new google.visualization.PieChart($('.pieChart').get(0))
+
 	google.visualization.events.addListener(googleTable, 'select', selectHandler);
 
 	function selectHandler(e) {
-		console.log(e)
+		var selectedId = $('.google-visualization-table-tr-sel').attr('_id')
+		searchFoodById(selectedId)
 	}
-	resetData()
-
+	resetTableData()
  	googleChartsDraw([])
+
+ 	resetPieData()
 }
 
 function googleChartsDraw(rows) {
-	googleData.addRows(rows)
-    googleTable.draw(googleData, {showRowNumber: true});
+	tableData.addRows(rows)
+    googleTable.draw(tableData, {showRowNumber: true});
 }
 
 // generate an array of arrays, with each element having fields matching queryDict['fields']
 function googleChartsUpdate(data) {
-	resetData()
+	resetTableData()
 	var allRows = []
 	var ids = []
 
@@ -123,6 +148,36 @@ function googleChartsUpdate(data) {
 	var d3Rows = d3.selectAll('.google-visualization-table-table tr:not(.google-visualization-table-tr-head)')
 	d3Rows.attr("_id", function(d, i){
 		return String(ids[i])})
+}
+
+// pie chart implementation
+function resetPieData() {
+	pieData = new google.visualization.DataTable()
+ 	pieData.addColumn('string', 'Name')
+ 	pieData.addColumn('number', 'Data')
+}
+
+function googlePieDraw(rows) {
+	pieData.addRows(rows)
+	googlePieChart.draw(pieData);
+}
+
+function googlePieUpdate(data) {
+	console.log(data)
+	console.log(data[2])
+	console.log(data.item_id)
+	console.log(data['nf_calories'])
+	resetPieData()
+	var allRows = []
+	for (var i = 0; i < nutrition.length; i++) {
+		// fieldName is a string, like "nf_calories"
+		var fieldName = nutrition[i]
+		var row = []
+		row[0] = fieldName
+		row[1] = data[fieldName]
+		allRows[i] = row
+	}
+	googlePieDraw(allRows)
 }
 
 // event listener
