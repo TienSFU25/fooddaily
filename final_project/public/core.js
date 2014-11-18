8ffec9a1f6f102638f8bd965047bde82d7caa3db
dkorkut <dkorkut@sfu.ca>
2014-11-17 22:32:10 -0800

    Added bootstrap, added csrf integration for angularjs, new index page

---
+++ final_project/public/core.js
@@ -0,0 +1,41 @@
+var foodList = angular.module('foodList', []);
+
+function foodListController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all food list data and show them
+    $http.get('/api/foodlist')
+        .success(function(data) {
+            $scope.list = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.addFood = function() {
+        $http.post('/api/foodlist', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.list = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteFood = function(id) {
+        $http.delete('/api/foodlist/' + id)
+            .success(function(data) {
+                $scope.list = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+           });
+   };
+
+}
