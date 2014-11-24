fc1aef36dd58edbf04527b3f3d79e7331cf0e613
Tien <tienv@sfu.ca>
2014-11-23 18:18:42 -0800

    Change POST in eat food form to ajax. Show (sloppy) notification when food is added

--- final_project/public/javascript/magnificPopup.js
+++ final_project/public/javascript/magnificPopup.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 	$('.popup-with-form').magnificPopup({
 		type: 'inline',
-		preloader: false,
+		preloader: true, 
 		focus: '#name',
+		removalDelay: 300,
+		  mainClass: 'mfp-fade',
+		  callbacks: {
+		    open: function() {
+		      console.log('popup opened!')
+		    },
+		    close: function() {
+		    	console.log('popup closed')
+		    }
+		  }  
 	});
 })
