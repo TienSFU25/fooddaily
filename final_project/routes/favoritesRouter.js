c75b2c389a20eafa8f32bc4675f64e44c2527790
Duji <dtufail@sfu.ca>
2014-11-29 07:48:16 -0800

    page now links to favoritesRouter although there's a bug that needs to be fixed

--- final_project/routes/favoritesRouter.js
+++ final_project/routes/favoritesRouter.js
@@ -1,15 +1,40 @@
 // this class is used for the list page, after a user logs in
 
 var express = require('express')
-var _ = require('underscore')
 
 favoritesRouter = express.Router()
 
 var User = db.model('User')
+var FavRecipes = db.model('FavRecipes')
 
+// favoritesRouter.get('/', function(req, res) {
+// 	// getFavsCallback = function(err, rows) {
+// 	// 	if (!err) {
+// 	// 		console.log(req.user.username)
+// 	// 		res.render('favrecipes', {user:req.user, rows:rows, csrfToken: req.csrfToken()})
+// 	// 	} else {
+// 	// 		console.log(err)
+// 	// 	}
+// 	// }
+// 	// FavRecipes.getFavs(req.user.id, getFavsCallback);
+
+
+// 	FavRecipes.getFavs(req.user.id, function(err, rows) {
+// 			res.render('favrecipes', {user:req.user, rows:rows, csrfToken: req.csrfToken()})
+// 		});
+
+// 	// res.render('favrecipes')
+// })
+
+
+
+
+
+// Returns: TypeError: Object [object SequelizeModel] has no method 'checkFood'
 favoritesRouter.get('/', function(req, res) {
-	res.render('favrecipes')
-})
-
+	FavRecipes.getFavs(req.user.id, function(err, rows) {
+			res.render('favrecipes', {user:req.user, rows:rows, csrfToken: req.csrfToken()})
+	});
+});
 
 module.exports = favoritesRouter
