a1110e5af51acd35a53cdb6378f63fa397fe42f0
Milan Milosevic <milan@pe-1.(none)>
2014-11-30 00:16:50 -0800

    Add Images for recipes, add listener for recipes. (adding to FavRecipe db not functional)

--- final_project/routes/recipeRouter.js
+++ final_project/routes/recipeRouter.js
@@ -1,17 +1,29 @@
 // this class is used for the list page, after a user logs in
 
-var express = require('express')
+var express = require('express');
 
 
 
-recipeRouter = express.Router()
+recipeRouter = express.Router();
 
-var User = db.model('User')
-var FavRecipes = db.model('FavRecipes')
+var User = db.model('User');
+var FavRecipes = db.model('FavRecipes');
 
 recipeRouter.get('/', function(req, res) {
-			res.render('recipes', {user:req.user, csrfToken: req.csrfToken()})
-})
+	res.render('recipes', {user:req.user, csrfToken: req.csrfToken()})
+});
 
+recipeRouter.post('/', function(req, res) {
+	var id = req.body.chosenId;
+	// var 
+	// var 
+	// var 
+	// var 
 
-module.exports = recipeRouter
+	//createFav = function(userid, recipeName, yield, ingredientsList, URL, IMG_URL, callback) {
+	// db.createFav(...);
+
+	res.render('recipes', {user:req.user, csrfToken: req.csrfToken()})
+});
+
+module.exports = recipeRouter;
